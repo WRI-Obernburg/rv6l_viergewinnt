5d0b28cf1ebfb8cb4b13eadcf0853b685acc3707
Tim Arnold <tim.arnold@wri-obernburg.de>
2025-07-25 11:18:08 +0200

    init

---
+++ index.ts
@@ -0,0 +1,105 @@
+import * as net from 'net';
+import { XMLParser } from 'fast-xml-parser';
+import * as stream from 'stream';
+
+var client = new net.Socket();
+const parser = new XMLParser();
+const incommingStream = new stream.PassThrough();
+let globalMessageCounter = 0;
+client.connect(80, '192.168.2.1',async function () {
+	console.log('Connected');
+
+	// START SESSION	const getVariable = "<RSVCMD><clientStamp>123</clientStamp><symbolApi><readSymbolValue><name>IMOVE</name><prog>S:/PROG/4GEWINNT/4GEWINNT</prog></readSymbolValue></symbolApi></RSVCMD>"
+	// SEND COMMAND
+	const startSessionCommand = 'SYMTABLE_SESSION / \n';
+	client.write(startSessionCommand);
+
+	await initSymTable()
+
+
+	await writeVariableInProc("IMOVE", "1");
+
+	console.log(await readVariableInProc("IMOVE"));
+
+	setInterval(async () => {
+		//await writeVariableInProc("IMOVE", globalMessageCounter.toString());
+		console.log(await readVariableInProc("IMOVE"));
+	}, 200);
+
+	//const enableGripper = "<RSVCMD><clientStamp>123</clientStamp><symbolApi><writeSymbolValue><name>_IBIN_OUT[6]</name><value>0</value></writeSymbolValue></symbolApi></RSVCMD>";
+	//client.write(enableGripper);
+});
+
+client.on('data', function (data) {
+	console.log('Received: ' + data);
+
+	//seperate data string after </RSVRES> and process each
+	const dataString = data.toString();
+	const messages = dataString.split('</RSVRES>');
+	messages.forEach((message) => {
+		if (message.trim()) { // Check if the message is not empty
+			const completeMessage = message + '</RSVRES>';
+			const jsonObj = parser.parse(completeMessage);
+			incommingStream.write(JSON.stringify(jsonObj)); // Write the complete message to the stream
+		}
+	});
+	
+});
+
+client.on('close', function () {
+	console.log('Connection closed');
+});
+
+
+async function readVariableInProc(name: string): Promise<string> {
+	let messageId = getNextMessageId();
+	const getVariable = `<RSVCMD><clientStamp>${messageId}</clientStamp><symbolApi><readSymbolValue><name>IMOVE</name><prog>S:/PROG/4GEWINNT/4GEWINNT</prog></readSymbolValue></symbolApi></RSVCMD>`
+	client.write(getVariable);
+
+	//check the next time a response comes in with the correct messageId
+	//ignore messages with other messageIds
+
+	const result = await waitForMessage(messageId);
+
+	return result.RSVRES.symbolApi.readSymbolValue.value;
+}
+
+async function initSymTable() {
+	let messageId = getNextMessageId();
+	const initSymbolsCommand = `<RSVCMD><clientStamp>${messageId}</clientStamp><symbolApi><initSymbolTable/></symbolApi></RSVCMD>`;
+	client.write(initSymbolsCommand);
+
+	await waitForMessage(messageId); // Wait for the response to ensure the write was successful
+
+}
+
+
+async function writeVariableInProc(name: string, value: string) {
+	let messageId = getNextMessageId();
+	const setVariable = `<RSVCMD><clientStamp>${messageId}</clientStamp><symbolApi><writeSymbolValue><name>${name}</name><prog>S:/PROG/4GEWINNT/4GEWINNT</prog><value>${value}</value></writeSymbolValue></symbolApi></RSVCMD>`;
+	client.write(setVariable);
+
+	await waitForMessage(messageId); // Wait for the response to ensure the write was successful
+
+}
+
+async function waitForMessage(id: number):Promise<any> {
+	return new Promise((resolve, reject) => {
+		const onDataCallback = (data: any) => {
+			const jsonObj = JSON.parse(data.toString());
+			if(jsonObj.RSVRES.clientStamp !== id) {
+				return; // Ignore messages with different clientStamp
+			}
+			resolve(jsonObj);
+			incommingStream.off('data', onDataCallback); // Remove the listener after resolving
+		};
+		incommingStream.on('data', onDataCallback);
+	});
+}
+
+function getNextMessageId(): number {
+	if(globalMessageCounter >= 1000) {
+		globalMessageCounter = 0;
+	}
+	return globalMessageCounter++;
+}
